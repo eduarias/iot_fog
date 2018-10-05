d4509a04d79bce0d55c9e47ff5d7565aa4b18699
Eduardo Arias <eduarias@users.noreply.github.com>
2018-10-05 14:56:46 +0200

    fix: closing the application did not close devices

--- cloud_connector/runner.py
+++ cloud_connector/runner.py
@@ -237,6 +237,8 @@ if __name__ == '__main__':
 
     try:
         runner.start()
+        while True:
+            time.sleep(1000)
     except (KeyboardInterrupt, TypeError, KeyError):
         runner.stop()
     except socket.error:
